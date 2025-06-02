7a1ccfdd48c092276bc068a025bf07cb59fba23c
elhard <elhardmarouan15@gmail.com>
2025-06-02 13:04:18 +0100

    Update urls.py

--- chats/urls.py
+++ chats/urls.py
@@ -1,10 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import MessageViewSet
-
-router = DefaultRouter()
-router.register(r'messages', MessageViewSet)
+from django.urls import path
+from .views import hello
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('hello/', hello),
 ]
