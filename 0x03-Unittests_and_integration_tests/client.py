741783f349e3eccc9fca3cc5a49d62eecc1af48c
elhard <elhardmarouan15@gmail.com>
2025-06-04 11:56:13 +0100

    Update client.py

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -2,13 +2,9 @@
 from utils import get_json
 
 class GithubOrgClient:
-    """Github org client."""
-
     def __init__(self, org_name):
         self.org_name = org_name
 
     @property
     def org(self):
-        """Return the org info by calling get_json."""
         return get_json(f"https://api.github.com/orgs/{self.org_name}")
-
