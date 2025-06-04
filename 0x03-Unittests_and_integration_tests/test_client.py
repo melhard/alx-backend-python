bb230414f6a9147593d87f2310f1dfcb9ed98cb4
elhard <elhardmarouan15@gmail.com>
2025-06-04 11:48:36 +0100

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,26 +1,18 @@
 #!/usr/bin/env python3
-"""اختبارات لوحدة GithubOrgClient"""
-
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
 from client import GithubOrgClient
 
-
 class TestGithubOrgClient(unittest.TestCase):
-    """اختبار GithubOrgClient"""
-
     @parameterized.expand([
         ("google", {"login": "google"}),
         ("abc", {"login": "abc"}),
     ])
-    @patch("client.get_json")  # تأكد من صحة المسار هنا
+    @patch("client.get_json")
     def test_org(self, org_name, expected, mock_get_json):
-        """اختبار خاصية org"""
         mock_get_json.return_value = expected
-
         client = GithubOrgClient(org_name)
         result = client.org
-
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(result, expected)
