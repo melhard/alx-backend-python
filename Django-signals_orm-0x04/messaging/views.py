fb527e9cb9cd99110030538ebdd5400f5443dade
elhard <elhardmarouan15@gmail.com>
2025-06-11 13:06:44 +0100

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from .models import Message
 
 def unread_messages_view(request):
-    unread_messages = Message.unread.unread_for_user(request.user)
+    unread_messages = Message.objects.filter(
+        receiver=request.user,
+        read=False
+    ).only('sender__username', 'content', 'timestamp').select_related('sender')
+
     return render(request, 'unread_messages.html', {
         'messages': unread_messages
     })
