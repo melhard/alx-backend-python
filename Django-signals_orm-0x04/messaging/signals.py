3e4213e5c91d1b3c7a27fcd5640fb8338de1d606
elhard <elhardmarouan15@gmail.com>
2025-06-11 11:29:38 +0100

    Update signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,9 +1,15 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, MessageHistory
 
-# إنشاء إشعار تلقائي عند استلام رسالة جديدة
-@receiver(post_save, sender=Message)
-def create_notification_on_new_message(sender, instance, created, **kwargs):
-    if created:
-        Notification.objects.create(user=instance.receiver, message=instance)
+# إشارة لتسجيل المحتوى القديم قبل التعديل
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:  # يعني أن الرسالة موجودة بالفعل
+        old_message = Message.objects.get(pk=instance.pk)
+        if old_message.content != instance.content:
+            MessageHistory.objects.create(
+                message=instance,
+                old_content=old_message.content
+            )
+            instance.edited = True  # تعيين أنها معدّلة
