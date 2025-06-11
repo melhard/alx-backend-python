72b690750b5b242f43c0f921298cbb761f6280bf
elhard <elhardmarouan15@gmail.com>
2025-06-11 13:09:55 +0100

    Update models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .managers import UnreadMessagesManager  # ⬅ استدعاء المدير المخصص
+from .managers import UnreadMessagesManager
 
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
