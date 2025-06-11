09f0c7be4ba3870440e306a779ddf14c056f05b5
elhard <elhardmarouan15@gmail.com>
2025-06-11 13:38:09 +0100

    Create views.py

---
+++ Django-signals_orm-0x04/messaging_app/messaging_app/chats/views.py
@@ -0,0 +1,9 @@
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from django.shortcuts import render
+from .models import Message
+
+@cache_page(60)  # 60 ثانية
+def conversation_messages(request, conversation_id):
+    messages = Message.objects.filter(conversation_id=conversation_id).order_by('timestamp')
+    return render(request, 'messages/conversation.html', {'messages': messages})
