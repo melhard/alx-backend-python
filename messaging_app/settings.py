dff99322e416c68b11fbc23c8b8452ec64398a52
elhard <elhardmarouan15@gmail.com>
2025-06-28 12:20:03 +0100

    Create settings.py

---
+++ messaging_app/settings.py
@@ -0,0 +1,12 @@
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+    }
+}
